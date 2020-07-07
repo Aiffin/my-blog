3ff74101deea0cbc8d5fcb25b8350563b432fec5
aiffin <aiffinsk@gmail.com>
2020-07-07 21:32:19 +0530

    First

---
+++ src/build/precache-manifest.ee9ca1dcf47439e2bd9c53542f74f073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743f58671d3b433a35e11b0e1440e994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d8160a97a348635239",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6964e529fca8597d6723",
+    "url": "/static/js/2.1de6057c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1de6057c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d8160a97a348635239",
+    "url": "/static/js/main.85466463.chunk.js"
+  },
+  {
+    "revision": "a47126a1c49d7af7aa53",
+    "url": "/static/js/runtime-main.ff0d576d.js"
+  },
+  {
+    "revision": "18d98af1128833f1a6daf4a264137a66",
+    "url": "/static/media/seaImage.18d98af1.jpg"
+  }
+]);
